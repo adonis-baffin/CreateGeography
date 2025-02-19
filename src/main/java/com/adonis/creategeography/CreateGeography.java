b26ba76227002a4a1d0b394012d9dfb0286d8468
adonis-baffin <3329586162@qq.com>
2025-02-19 20:52:26 +0800

    新的开始

---
+++ src/main/java/com/adonis/creategeography/CreateGeography.java
@@ -0,0 +1,15 @@
+package com.adonis.creategeography;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(CreateGeography.MODID)
+public class CreateGeography {
+    public static final String MODID = "creategeography";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public CreateGeography(){
+
+    }
+}
---
+++ src/main/java/com/adonis/mixin/ExampleMixin.java
@@ -0,0 +1,10 @@
+package com.adonis.mixin;
+
+import net.minecraft.world.item.BlockItem;
+import org.spongepowered.asm.mixin.Mixin;
+
+
+@Mixin(BlockItem.class) // 具体目标类
+public class ExampleMixin {
+    // Mixin 方法
+}
